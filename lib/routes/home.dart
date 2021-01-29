f39feb08c79d4c62e0706251ffc0d78fa5560550
t-sakurai816 <53470730+t-sakurai816@users.noreply.github.com>
2021-01-29 16:42:34 +0900

    TabBarを描画

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
 import '../header.dart';
-import '../header_drawer.dart';
+import 'home2.dart';
+
+// import '../header_drawer.dart';
+// class TabInfo {
+//   String label;
+//   Widget widget;
+//   TabInfo(this.label, this.widget);
+// }
+
+// final List<TabInfo> _tabs = [
+//   TabInfo("HOME1", Home()),
+//   TabInfo("HOME2", Home2()),
+// ];
+
+final List<Tab> myTabs = <Tab>[
+  Tab(text: 'LEFT'),
+  Tab(text: 'RIGHT'),
+];
 
 class Home extends StatelessWidget {
   final String screenName = 'ホーム'; //headerに表示される名前
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: HeaderDrawer(),
-      appBar: Header(headerTitle: screenName),
-      body: Center(
-        child: Text(screenName),
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        // drawer: HeaderDrawer(),
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: myTabs,
+          ),
+        ),
+        body: TabBarView(
+          children: myTabs.map((Tab tab) {
+            final String label = tab.text.toLowerCase();
+            return Center(
+              child: Text(
+                'This is the $label tab',
+                style: const TextStyle(fontSize: 36),
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
