38246707853add9e28f4b212a8c3703204c95a7f
t-sakurai816 <53470730+t-sakurai816@users.noreply.github.com>
2021-01-31 16:32:11 +0900

    投稿部分を表示

--- lib/routes/home/home1.dart
+++ lib/routes/home/home1.dart
@@ -6,7 +6,8 @@ class Home1 extends StatelessWidget {
 
   final Map _tweetsData = {
     'Name': 'ツイート太郎',
-    'text': 'ビールがうまい',
+    'text':
+        'ビールがうまい。ビールがうまい。ビールがうまい。ビールがうまい。ビールがうまい。ビールがうまい。ビールがうまい。ビールがうまい。ビールがうまい。ビールがうまい。',
     'timestamp': '3m'
   };
   @override
@@ -16,22 +17,93 @@ class Home1 extends StatelessWidget {
         child: ListView.builder(
           itemBuilder: (BuildContext context, int index) {
             return Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  // NetworkImageでURLの画像を取得
-                  backgroundImage: NetworkImage(
-                      'https://pbs.twimg.com/profile_images/1348572281681760262/sVPExbGK.jpg'),
+              child: Container(
+                child: Column(
+                  children: [
+                    Container(
+                      child: Container(
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            // NetworkImageでURLの画像を取得
+                            backgroundImage: NetworkImage(
+                                'https://pbs.twimg.com/profile_images/1348572281681760262/sVPExbGK.jpg'),
+                          ),
+                          title: Text(_tweetsData['Name']), //ユーザーの名前
+                          subtitle:
+                              Text(_tweetsData['text']), //投稿の本文(勝手に縦幅が変わる)
+                          trailing: Text(_tweetsData['timestamp']),
+                          isThreeLine: true, // Three-lineを使用する
+                          onTap: () {
+                            print('タップしたよ');
+                          },
+                          onLongPress: () {
+                            print('長押ししたよ');
+                          },
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: NewWidget(),
+                    ),
+                  ],
                 ),
-                title: Text(_tweetsData['Name']), //ユーザーの名前
-                subtitle: Text(_tweetsData['text']), //投稿の本文(勝手に縦幅が変わる)
-                trailing: Text(_tweetsData['timestamp']),
-                isThreeLine: true, // Three-lineを使用する
               ),
             );
           },
           itemCount: _tweetsNum,
         ),
       ),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      child: ButtonBar(
+          buttonPadding: EdgeInsets.all(0.0),
+          alignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.chat_bubble_outline),
+                  iconSize: 18.0,
+                  onPressed: () {},
+                ),
+                Text(''),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.cached),
+                  iconSize: 18.0,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.favorite_border),
+                  iconSize: 18.0,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            IconButton(
+              icon: Icon(Icons.share),
+              iconSize: 18.0,
+              onPressed: () {},
+            ),
+          ]),
     );
   }
 }
