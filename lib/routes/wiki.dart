85a1271efc1309d13a07c71de0270cc831052aff
t-sakurai816 <53470730+t-sakurai816@users.noreply.github.com>
2021-02-01 02:39:44 +0900

    画像をGrid表示

--- lib/routes/wiki.dart
+++ lib/routes/wiki.dart
@@ -7,8 +7,17 @@ class Wiki extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Header(headerTitle: screenName),
-      body: Center(
-        child: Text(screenName),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(50, (index) {
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Image.network(
+                  'https://pbs.twimg.com/profile_images/1348572281681760262/sVPExbGK.jpg'),
+            ),
+          );
+        }),
       ),
     );
   }
